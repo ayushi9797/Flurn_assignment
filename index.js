fd01714d608405096bd396a2ff6a0546d7846c84
ayushi9797 <soniayushi345@gmail.com>
2023-06-19 15:51:32 +0530

    index.js file with imported routers

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-"node"
-"docker"
+const express = require("express");
+const { connection } = require("./config/db");
+const { SeatRouter } = require("./router/seat.router");
+const { PriceRouter } = require("./router/price.router");
+const { BookingRouter } = require("./router/booking.router");
+
+const app = express();
+
+// ! Importing Routes Seats , price , Booking routes 
+
+app.use("/", SeatRouter);
+app.use("/", PriceRouter);
+app.use("/", BookingRouter);
+
+
+app.use(express.json());
+
+
+// ! Home Router
+
+app.get("/", async (req, res) => {
+  try {
+    res.send(`<h1>  WElcome to Home Router   </h1>`);
+
+  } catch (error) {
+    console.log(error.message);
+
+  }
+
+})
+
+
+
+
+
+// ! connection to server
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log(`app listening on port ${process.env.port}`);
+  } catch (error) {
+    console.log({ error: `error in connections with the  port: ${error.message}` });
+  }
+});
