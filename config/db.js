c783ac59c31373c6013f38b396e4f6f06e012c77
ayushi9797 <soniayushi345@gmail.com>
2023-06-19 15:55:59 +0530

    database connection file

---
+++ config/db.js
@@ -0,0 +1,9 @@
+// ! Making Connection with Database
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = mongoose.connect(process.env.mongo_url);
+console.log(`connected to  mongo database successfully.`);
+module.exports = {
+  connection,
+};
